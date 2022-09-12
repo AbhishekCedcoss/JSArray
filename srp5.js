2346bc433481e05b9d20a2877d4db5259f91cef6
AbhishekCedcoss <abhishekmishra2022aug@cedcoss.com>
2022-09-12 12:56:03 +0530

    task 6

--- srp5.js
+++ srp5.js
@@ -1,2 +1,112 @@
-var prod =document.getElementById("pro").value;
-console.log(prod)
+var obj1 = {
+  Company: "Samsung",
+  model: "Galaxy",
+  Memory: "64",
+  Price: "15000",
+};
+var obj2 = {
+  Company: "Nokia",
+  model: "S730",
+  Memory: "128",
+  Price: "22000",
+};
+var obj3 = {
+  Company: "Xiaomi",
+  model: "Note",
+  Memory: "32",
+  Price: "12000",
+};
+var obj4 = {
+  Company: "Motorola",
+  model: "G10",
+  Memory: "32",
+  Price: "15000",
+};
+var obj5 = {
+  Company: "Apple",
+  model: "S12",
+  Memory: "64",
+  Price: "25000",
+};
+var ar = [obj1, obj2, obj3, obj4, obj5];
+
+var ar1=[]
+function fun1() {
+    var prod = document.getElementById("pro").value;
+    var val = document.getElementById("val").value;
+    if (prod==="Samsung Galaxy") {
+        ar1.push(prod)
+        ar1.push(val)
+        ar1.push(val*15000) 
+        
+        console.log(ar1)
+        
+      }
+      if (prod==="Nokia S730") {
+        ar1.push(prod)
+        ar1.push(val)
+        ar1.push(val*22000) 
+        
+        console.log(ar1)
+        
+      }
+      if (prod==="Xiaomi Note") {
+        ar1.push(prod)
+        ar1.push(val)
+        ar1.push(val*12000) 
+        
+        console.log(ar1)
+        
+      }
+      if (prod==="Motorola G10") {
+        ar1.push(prod)
+        ar1.push(val)
+        ar1.push(val*15000) 
+        
+        console.log(ar1)
+        
+      }
+      if (prod==="Apple S12") {
+        ar1.push(prod)
+        ar1.push(val)
+        ar1.push(val*25000) 
+        
+        console.log(ar1)
+        
+      }
+//       for (let i = 0; i < ar1.length; i++) {
+//         var ele = ar1[i];
+//         console.log(ar1[i])<th id ='tdh'>Action</th>
+// //   console.log(prod);<th id ='tdh'>Action</th>
+//   console.log(val);
+}
+function fun2() {
+    var tab="<h2>Bill</h2><div><table border=1><tr><th id ='tdh'>Description</th><th id ='tdh'>Quantity</th> <th id ='tdh'>Amount</th>";
+    document.getElementById("table").innerHTML = tab;
+    sum=0
+
+    for (let i = 0; i < ar1.length; i++){
+        tab +=
+          "<tr><td id='tds'>" +
+          ar1[i] +
+          "</td id='tds'><td id='tds'>" +
+          ar1[i+1] +
+          "</td> <td id='tds'>" +
+         ar1[i+2] +    
+          "</td></tr>"
+          
+          sum+=ar1[i+2]
+          i+=2;
+          
+          
+      };
+      console.log(sum)
+      
+      tab += "</div></table>";
+      tab+="<h4> Total Amount :"+sum+"</h4>"
+
+      document.getElementById("table").innerHTML = tab;
+
+   
+   
+}
--- srp6.js
+++ srp6.js
@@ -35,9 +35,62 @@ var obj1 = {
     pos:"<input type='checkbox' id='acb'  value='box'> "
   };
   var ar = [obj1, obj2, obj3, obj4, obj5];
+
   function chk() {
-    var x = document.getElementById("acb").checked;
-    console.log(x)
+    var x1 = document.getElementById("scb").checked;
+    var x2 = document.getElementById("ncb").checked;
+    var x3 = document.getElementById("xcb").checked;
+    var x4 = document.getElementById("mcb").checked;
+    var x5 = document.getElementById("acb").checked;
+    console.log(x1)
+    if (x1===true) {
+      
+      delete ar[0]
+      
+      
+    }
+    if (x2===true) {
+      delete ar[1]
+      
+      
+    }
+    if (x3===true) {
+      delete ar[2]
+      
+      
+    }
+    if (x4===true) {
+      delete ar[3]
+     
+      
+    }
+    if (x5===true) {
+      delete ar[4]
+      
+      
+    }
+    var fsrc=
+    "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th>";
+    document.getElementById("fres").innerHTML = fsrc;
+    ar.forEach((element) => {
+      
+      fsrc +=
+        "<tr><td id='tds'>" +
+        element.Company +
+        "</td id='tds'><td id='tds'>" +
+        element.model +
+        "</td> <td id='tds'>" +
+        element.Memory +
+        "</td> <td id='tds'>" +
+        element.Price +
+        "</td> </tr>";
+        document.getElementById("fres").innerHTML = fsrc;
+    });
+    
+    fsrc += "</div></table>";
+
+    console.log(ar)
+   
   }
   function t1() {
     var src =
