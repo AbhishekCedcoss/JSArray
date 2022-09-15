3dc29eb4c49c29207807aa8748c4791ab5f336a5
AbhishekCedcoss <abhishekmishra2022aug@cedcoss.com>
2022-09-15 10:50:52 +0530

    Task -9

--- srp4.js
+++ srp4.js
@@ -1,67 +1,62 @@
-var obj1 = {
-  company: "Samsung",
-  model: "Galaxy",
-  memory: "64",
-  price: "15000",
-};
-var obj2 = {
-  Company: "Nokia",
-  model: "S730",
-  memory: "128",
-  price: "22000",
-};
-var obj3 = {
-  company: "Xiaomi",
-  model: "Note",
-  memory: "32",
-  price: "12000",
-};
-var obj4 = {
-  company: "Motorola",
-  model: "G10",
-  memory: "32",
-  price: "15000",
-};
-var obj5 = {
-  company: "Apple",
-  model: "S12",
-  memory: "64",
-  price: "25000",
-};
-var obj6 = {
-  company: "temp",
-  model: "temp",
-  memory: "temp",
-  price: "temp",
-};
-ar = [obj1, obj2, obj3, obj4, obj5, obj6];
-function addfun() {
-  var company = document.getElementById("company").value;
-  var model = document.getElementById("model").value;
-  var memory = document.getElementById("memory").value;
-  var price = document.getElementById("price").value;
-  obj6.company = company;
-  obj6.model = model;
-  obj6.memory = memory;
-  obj6.price = price;
-}
-function t1() {
-  var src =
-    "<div><table border=1><tr><th id ='tdh'>Company</th><th id ='tdh'>Model</th> <th id ='tdh'>Memory(GB)</th><th id ='tdh'>Price(RS)</th>";
-  document.getElementById("res").innerHTML = src;
-  ar.forEach((element) => {
-    src +=
-      "<tr><td id='tds'>" +
-      element.company +
-      "</td id='tds'><td id='tds'>" +
-      element.model +
-      "</td> <td id='tds'>" +
-      element.memory +
-      "</td> <td id='tds'>" +
-      element.price +
-      "</td></tr>";
-    document.getElementById("res").innerHTML = src;
-  });
+var data = [
+  {
+    Company: "Samsung",
+    Model: "Galaxy",
+    Memory: 64,
+    Price: 15000,
+  },
+  {
+    Company: "Nokia",
+    Model: "S730",
+    Memory: 128,
+    Price: 22000,
+  },
+  {
+    Company: "Xiaomi",
+    Model: "Note",
+    Memory: 32,
+    Price: 12000,
+  },
+  {
+    Company: "Motorala",
+    Model: "G10",
+    Memory: 32,
+    Price: 15000,
+  },
+  {
+    Company: "Apple",
+    Model: "S12",
+    Memory: 64,
+    Price: 25000,
+  },
+];
 
-  src += "</div></table>";
+
+function fun() {
+  var cmp=document.getElementById("cmp").value;
+  var mdl=document.getElementById("mdl").value;
+  var mem=document.getElementById("mem").value;
+  var pri=document.getElementById("pri").value;
+
+  var val={
+      Company:cmp,
+      Model:mdl,
+      Memory:mem,
+      Price:pri
+  }
+  data.push(val);
+  var table="<table><tr><th>Company</th><th>Model</th><th>Memory(GB)</th><th>Price(RS)</th></tr>";
+  data.forEach(v => {
+      table+="<tr><td>"+
+      v.Company+
+      "</td><td>"+
+      v.Model+
+      "</td><td>"+
+      v.Memory+
+      "</td><td>"+
+      v.Price+
+      "</td></tr>"
+  });
+  table+="</table>";
+  document.getElementById("output").innerHTML=table;
 }
